eb0d2bf56527a6624fdc82b2f0c840dc1399b9dd
Emily <emelizadaniels@gmail.com>
2022-12-13 11:05:39 -0500

    commit tree 31aa3a8d19690112c161eea3d46060850d4f243d

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,8 +1,14 @@
+require 'byebug'
 class CheesesController < ApplicationController
 
   def index
     cheeses = Cheese.all
     render json: cheeses
+  end
+  
+  def show
+    cheese = Cheese.find_by(id: params[:id])
+    render json: cheese
   end
 
 end
